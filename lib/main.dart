3b70b2466f897da3021dc30bc496e5e96ce50980
shenioalves <shenioalvesjgs@gmail.com>
2025-09-21 10:24:53 -0300

    tela de splash e home chekin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hackathon_plus/features/game/view/dino_game_view.dart';
+import 'package:hackathon_plus/features/home/view/home_checkin.dart';
+
+import 'package:hackathon_plus/features/splash/view/splash_view.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,9 +10,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DinoGameScreen(),
+      initialRoute: '/',
+
+      // SEU MAPA DE ROTAS ESTÁ CORRETO
+      routes: {
+        '/': (context) => SplashView(), // É uma boa prática usar builders
+        '/home_checkin': (context) => HomeCheckin(),
+      },
     );
   }
 }

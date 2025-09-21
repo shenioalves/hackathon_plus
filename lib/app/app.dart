d7e03e541032304b4c5d99529c02d9af3d4b547c
shenioalves <shenioalvesjgs@gmail.com>
2025-09-20 22:10:22 -0300

    init commit

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:hackathon_plus/app/home_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeView(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hackathon_plus/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
